261dadb5978b79da0ae901d5c9fa470853625ef9
bryan-lolordo <134180762+bryan-lolordo@users.noreply.github.com>
2023-06-03 12:56:44 -0500

    Create pre_clean_exploration.sql

---
+++ pre_clean_exploration.sql
@@ -0,0 +1,31 @@
+/*
+Comments
+*/
+
+CREATE TABLE `divvy-app-data.bike_tripdata.combined_tripdata` AS
+SELECT *
+FROM (
+    SELECT * FROM `divvy-app-data.bike_tripdata.jan2022_tripdata`
+    UNION ALL
+    SELECT * FROM `divvy-app-data.bike_tripdata.feb2022_tripdata`
+    UNION ALL
+    SELECT * FROM `divvy-app-data.bike_tripdata.mar2022_tripdata`
+    UNION ALL
+    SELECT * FROM `divvy-app-data.bike_tripdata.apr2022_tripdata`
+    UNION ALL
+    SELECT * FROM `divvy-app-data.bike_tripdata.may2022_tripdata`
+    UNION ALL
+    SELECT * FROM `divvy-app-data.bike_tripdata.june2022_tripdata`
+    UNION ALL
+    SELECT * FROM `divvy-app-data.bike_tripdata.jul2022_tripdata`
+    UNION ALL
+    SELECT * FROM `divvy-app-data.bike_tripdata.aug2022_tripdata`
+    UNION ALL
+    SELECT * FROM `divvy-app-data.bike_tripdata.sept2022_tripdata`
+    UNION ALL
+    SELECT * FROM `divvy-app-data.bike_tripdata.oct2022_tripdata`
+    UNION ALL
+    SELECT * FROM `divvy-app-data.bike_tripdata.nov2022_tripdata`
+    UNION ALL
+    SELECT * FROM `divvy-app-data.bike_tripdata.dec2022_tripdata`
+);
